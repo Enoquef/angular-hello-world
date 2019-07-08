a1e0128ba116f4e274392f46426db334fba7958a
enoque fernandes <nettolinhares18@gmail.com>
2019-07-08 16:28:12 -0300

    news-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MensagemComponent } from './mensagem/mensagem.component';
+import { Mensagem1Component } from './mensagem1/mensagem1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MensagemComponent
+    MensagemComponent,
+    Mensagem1Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/mensagem1/mensagem1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:'app-mensagem1',
+    templateUrl:'./mensagem1.component.html',
+    styleUrls: ['./mensagem1.component.css']
+})
+export class Mensagem1Component{
+
+}
