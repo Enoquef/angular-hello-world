ac8000db8cf550408d04140d7bba9d06c9b20452
enoque fernandes <nettolinhares18@gmail.com>
2019-07-11 00:00:15 -0300

    users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MensagemComponent } from './mensagem/mensagem.component';
 import { Mensagem1Component } from './mensagem1/mensagem1.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MensagemComponent,
-    Mensagem1Component
+    Mensagem1Component,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule
